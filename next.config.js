d3c59e61891460e056d3fd52f599d957c6b72d3b
Tran Minh Nghia <nghiatm202@gmail.com>
2023-10-18 15:26:44 +0700

    add: next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
   // distDir: "build",
+  output: "export",
   swcMinify: true,
   reactStrictMode: true,
   compiler: {
